cecf5c3457e097e9e09074dfd46f1d90b8eb4811
Thiago Prazeres <thiagoprazeres@gmail.com>
2026-05-01 04:46:43 -0300

    Configure database SSL for Supabase

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       autoLoadEntities: true,
       synchronize: true,
